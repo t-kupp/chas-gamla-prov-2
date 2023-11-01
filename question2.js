d317c356050df17bf88330f8df805d51d4f35594
David Shore <david@foggymedia.com>
2023-11-01 13:48:30 +0100

    -

--- question2.js
+++ question2.js
@@ -6,3 +6,4 @@ const numbers = [200, 54, 582, 33, 4];
 // 2.2 (2p) Använd map på arrayen numbers2 och
 // skapa en ny array där alla tal är tripplade. Alltså gånger 3.
 // Den nya arrayen ska sparas i en variabel som heter tripple.
+const numbers2 = [200, 54, 582, 33, 4];
